473f9d58fbe26f5d9181988016377e928b1c92b6
prerna1407 <prerna.pandey879@gmail.com>
2017-06-28 01:40:28 +0530

    Runs only root as a user. Displays message if not

--- first.sh
+++ first.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # insert/update hosts entry
+
 echo "Enter the ip address"
 read ip_address
 echo "Enter the hostname"
 read host_name
 host_entry="${ip_address} ${host_name}"
 
-echo "Please enter your password if requested."
-
-
     echo "Adding new hosts entry."
-    echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
-
+    if  echo "$host_entry" | tee -a /etc/hosts > /dev/null
+    then
+    echo "Succesfully Done"
+    else
+    echo "make sure you run this as root user" 
+    fi
