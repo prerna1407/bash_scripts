a5dd8f1baab2521a6ba24c026e866f7823e30544
prerna1407 <prerna.pandey879@gmail.com>
2017-06-28 14:31:32 +0530

    main script will only run if the user is root.

--- first.sh
+++ first.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
-# insert/update hosts entry
-
-echo "Enter the ip address"
-read ip_address
-echo "Enter the hostname"
-read host_name
-host_entry="${ip_address} ${host_name}"
-
-    echo "Adding new hosts entry."
-    if  echo "$host_entry" | tee -a /etc/hosts > /dev/null
-    then
-    echo "Succesfully Done"
-    else
-    echo "make sure you run this as root user" 
-    fi
+#make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+	   echo "This script must be run as root" 1>&2
+	      exit 1
+      else
+#insert the values for ip and host
+	      echo "Enter the ip address"
+	      read ip_address
+	      echo "Enter the hostname"
+	      read host_name
+	      host_entry="${ip_address} ${host_name}"
+	      echo "Adding new hosts entry."
+	      echo "$host_entry" | tee -a /etc/hosts > /dev/null
+	      echo "Succesfully Done" 
+fi
