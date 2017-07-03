0662e422a70876a51a79abfac74f5b5abbfd988f
prerna1407 <prerna.pandey879@gmail.com>
2017-07-03 18:16:29 +0530

    Script to add hostname to /etc/hosts file

--- first.sh
+++ first.sh
@@ -5,12 +5,12 @@ if [ "$(id -u)" != "0" ]; then
 	      exit 1
       else
 #insert the values for ip and host
-	      echo "Enter the ip address"
-	      read ip_address
-	      echo "Enter the hostname"
-	      read host_name
-	      host_entry="${ip_address} ${host_name}"
-	      echo "Adding new hosts entry."
-	      echo "$host_entry" | tee -a /etc/hosts > /dev/null
-	      echo "Succesfully Done" 
+      echo "Enter the ip address"
+      read ip_address
+      echo "Enter the hostname"
+      read host_name
+      host_entry="${ip_address} ${host_name}"
+      echo "Adding new hosts entry."
+      echo "$host_entry" | tee -a /etc/hosts > /dev/null
+      echo "Succesfully Done" 
 fi
