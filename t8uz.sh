5dbb8911d6d170484aa7b7cce192d5e68cabc4fe
prerna1407 <prerna.pandey879@gmail.com>
2017-07-06 17:22:19 +0530

    Script to create virtual host file

--- t8uz.sh
+++ t8uz.sh
@@ -1,60 +1,69 @@
 #!/bin/bash
-shopt -s xpg_echo
-if [ "$(id -u)" != "0" ]; then
-	   echo "This script must be run as root" 
-	      exit 1
-else
-
-        echo "Enter the domain name"
-        read name
-        if [ "$name" == "0" ]; then 
-	    echo "You need to supply at least one argument!" 
-	        exit 1
+if [ $(id -u) -eq 0 ];
+then
+	trydomain()
+	{
+	echo "Enter the domain name"
+	read domain
+	echo $domain | grep ^[a-zA-Z0-9][a-zA-Z0-9\-]*.*'[com|edu|in|com|\.in|org]$' >> /dev/null
+	if [ $? -eq 0 ];
+	then
+		grep -w $domain /etc/hosts >> /dev/null
+		if [ $? -eq 0 ];
+		then
+				clear
+				echo "Domain name already exists"
+				trydomain		
+			fi
+		else
+				clear
+				echo "Can't match the pattern"
+				trydomain
 	fi
-       echo "Enter the email"
-       read email
-       regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
-       if [[ $email =~ $regex ]] ; then
-	      echo "Valid Email Address"
-       else
-	      echo "Invalid email address, try again"
-	      exit 2
-       fi
-      dir_path="/var/www/$name/public_html"
+	}
+	trydomain
+    echo "Enter the email"
+    read email
+    regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+    if [[ $email =~ $regex ]] ; then
+	echo "Valid Email Address"
+    else
+	echo "Invalid email address, try again"
+	exit 2
+	fi
+    
 # Directory structure for virtual hosts
-      if [ -e /var/www/$name ]; then 
-	echo "Domain name already Exist"
-	exit 3
-      fi
-      mkdir -p $dir_path
+    dir_path="/var/www/$name/public_html"
+    mkdir -p $dir_path
 
 # granting permission
-      chown -R $USER:$USER $dir_path
-      chmod -R 755 /var/www
+    chown -R $USER:$USER $dir_path
+    chmod -R 755 /var/www
 
  # after this step, create a demo page using
-      touch $dir_path/index.html
-      echo "
-      <html>
-      <head>
-      <title> Welcome to my page </title>
-      </head>
-      <body>
-      <h1> Sucess! You're welcome here </h1>
-      </body>
-      </html>" > $dir_path/index.html
-      # creating virtual host file
-      apac_path="/etc/apache2/sites-available"
-      touch $apac_path/$name.conf
-      siteAvailabledomain="$apac_path/$name.conf"
-      echo "
-      <VirtualHost *:80>
-      ServerAdmin $email
-      DocumentRoot /var/www/$name/public_html
-      ServerName $name
-      ServerAlias www.$name.com
-      </VirtualHost>" >  $siteAvailabledomain
-      echo "####################"
-      echo "Virtual host created"
-
+    touch $dir_path/index.html
+    echo "
+    <html>
+    <head>
+    <title> Welcome to my page </title>
+    </head>
+    <body>
+    <h1> Sucess! You're welcome here </h1>
+    </body>
+    </html>" > $dir_path/index.html
+# creating virtual host file
+    apac_path="/etc/apache2/sites-available"
+    touch $apac_path/$name.conf
+    siteAvailabledomain="$apac_path/$name.conf"
+    echo "
+    <VirtualHost *:80>
+    ServerAdmin $email
+    DocumentRoot /var/www/$name/public_html
+    ServerName $name
+    ServerAlias www.$name.com
+    </VirtualHost>" >  $siteAvailabledomain
+    echo "####################"
+    echo "Virtual host created"
+else 
+	echo "User must be run as root!!"
 fi
